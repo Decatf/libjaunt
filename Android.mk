0302a789646c48bf6b77790edf79a904628110ac
ryang <decatf@gmail.com>
2017-02-08 09:04:00 -0500

    Reduce binary size

--- Android.mk
+++ Android.mk
@@ -2,15 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CLANG := false
-
-LOCAL_STRIP_MODULE := keep_symbols
-
 LOCAL_SRC_FILES := \
 	jaunt.c
 
 LOCAL_CFLAGS += -Werror -mcpu=cortex-a9 -mfloat-abi=softfp -mfpu=vfpv3-d16
-LOCAL_CFLAGS += -fpic
+LOCAL_CFLAGS += -fvisibility=hidden
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_MODULE := libjaunt
